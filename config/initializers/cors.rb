e0095ea0d09ee7642f8551ed9dcdd1f932c047a9
syaifulfu <msyaifulfuad25@gmail.com>
2022-06-07 09:03:33 +0700

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins "localhost:8080"
-    origins "localhost:8080", "localhost:8082"
+    origins "localhost:8080", "localhost:8082", "turbolyapp2-frontend.herokuapp.com"
     # origins "*"
 
     resource "*",
